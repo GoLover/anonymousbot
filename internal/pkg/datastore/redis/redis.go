cba9126c2d6ef7a5519c7c54a991e0c4fef11ed6
Bentlee <latebozorg@gmail.com>
2021-10-01 11:28:34 +0330

    [Fix] implement DSInterrface in redis package.

--- internal/pkg/datastore/redis/redis.go
+++ internal/pkg/datastore/redis/redis.go
@@ -3,3 +3,11 @@ type RedisDataStore struct {}
 func InitDataStore() RedisDataStore{
 	return RedisDataStore{}
 }
+
+func Get(key string) string {
+	return ""
+}
+
+func Set(key,value string) {
+	return ""
+}
